64fcbdc251e9c18dd23d3a7fe2c45fbe282fbdcc
MarinaShemesh <marina.shemesh1@gmail.com>
2017-05-20 13:47:48 +0300

    20May

---
+++ script.js
@@ -0,0 +1,20 @@
+  function showValue(newValue)
+      {
+        document.getElementById("range").innerHTML=newValue;
+      }
+
+
+
+
+
+function updateSlider(slideAmount) {
+//get the element
+  var display = document.getElementById("chosen")
+  //show the amount
+  display.innerHTML=slideAmount;
+  //get the element
+  var pic = document.getElementById("pic");
+  //set the dimensions
+  pic.style.width=slideAmount+"%";
+  pic.style.height=slideAmount+"%";
+}
